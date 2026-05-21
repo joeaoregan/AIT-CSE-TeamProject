6d1d27c3d89b48717d3ace067a780a82994fe5e5
joeaoregan <22237219+joeaoregan@users.noreply.github.com>
2026-05-21 20:38:20 +0100

    JSF Tables: add comments

--- JSF/Tables/src/com/ait/tables/Name.java
+++ JSF/Tables/src/com/ait/tables/Name.java
@@ -1,36 +1,41 @@
 package com.ait.tables;
 
+/**
+ * A simple class to hold a first and last name. This is used in the tables
+ * example.
+ */
 public class Name {
-	private String first;
-	private String last;
-	boolean canEdit;
-	
-	public Boolean getCanEdit() {
-		return canEdit;
-	}
-	
-	public void setCanEdit(Boolean canEdit) {
-		this.canEdit = canEdit;
-	}
-	
-	public Name(String first, String last) {
-		this.first = first;
-		this.last = last;
-	}
 
-	public String getFirst() {
-		return first;
-	}
+    private String first;
+    private String last;
+    boolean canEdit;
 
-	public void setFirst(String first) {
-		this.first = first;
-	}
+    public Boolean getCanEdit() {
+        return canEdit;
+    }
 
-	public String getLast() {
-		return last;
-	}
+    public void setCanEdit(Boolean canEdit) {
+        this.canEdit = canEdit;
+    }
 
-	public void setLast(String last) {
-		this.last = last;
-	}
+    public Name(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
 }
--- JSF/Tables/src/com/ait/tables/TableData.java
+++ JSF/Tables/src/com/ait/tables/TableData.java
@@ -6,67 +6,95 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+/**
+ * A simple class to hold a list of names. This is used in the tables example.
+ */
 @ManagedBean
 @SessionScoped
 public class TableData {
-	private ArrayList<Name> names;
-	private String firstName;
-	private String lastName;
-	
-	@PostConstruct
-	public void init() {
-		names = new ArrayList<Name>();
-		Name firstName = new Name("William","Dupont");
-		names.add(firstName);
-		Name secondName = new Name("Harry", "Styles");
-		names.add(secondName);
-		Name thirdName = new Name("Jon", "BonJovi");
-		names.add(thirdName);
-	}
-	
-	public ArrayList<Name> getNames() {
-		return names;
-	}
-	
-	public String editName(Name name) {
-		name.setCanEdit(true);
-		return null;
-	}
-	
-	public String saveAction() {
-		for (Name name: names) {
-			name.setCanEdit(false);
-		}
-		return null;
-	}
-	
-	public String deleteName(Name name) {
-		names.remove(name);
-		return null;
-	}
 
-	public String getFirstName() {
-		return firstName;
-	}
+    private ArrayList<Name> names;
+    private String firstName;
+    private String lastName;
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    @PostConstruct
+    public void init() {
+        names = new ArrayList<Name>();
+        Name firstName = new Name("William", "Dupont");
+        names.add(firstName);
+        Name secondName = new Name("Harry", "Styles");
+        names.add(secondName);
+        Name thirdName = new Name("Jon", "BonJovi");
+        names.add(thirdName);
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public ArrayList<Name> getNames() {
+        return names;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	public String addName() {
-		final Name name = new Name(this.firstName,this.lastName);
-		
-		names.add(name);
-		firstName=null;
-		lastName=null;
-		return null;
-	}
+    /**
+     * Set the canEdit property to true for the name passed in. This will cause
+     * the name to be rendered as an input field in the table.
+     *
+     * @param name The name to edit
+     * @return null to stay on the same page
+     */
+    public String editName(Name name) {
+        name.setCanEdit(true);
+        return null;
+    }
+
+    /**
+     * Set the canEdit property to false for all names in the list. This will
+     * cause all names to be rendered as plain text in the table.
+     *
+     * @return null to stay on the same page
+     */
+    public String saveAction() {
+        for (Name name : names) {
+            name.setCanEdit(false);
+        }
+        return null;
+    }
+
+    /**
+     * Remove the name passed in from the list of names.
+     *
+     * @param name The name to delete
+     * @return null to stay on the same page
+     */
+    public String deleteName(Name name) {
+        names.remove(name);
+        return null;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Add a new name to the list of names.
+     *
+     * @return null to stay on the same page
+     */
+    public String addName() {
+        final Name name = new Name(this.firstName, this.lastName);
+
+        names.add(name);
+        firstName = null;
+        lastName = null;
+        return null;
+    }
 }
